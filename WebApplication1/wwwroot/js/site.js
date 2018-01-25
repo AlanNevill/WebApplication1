0d466aac9146c011a9bde3614944186939a83991
Alan Nevill <alannevill@outlook.com>
2018-01-25 14:39:30 +0000

    MGF v3.0

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-// Write your Javascript code.
+// site.js
+
+
+// Year End Calendar fix
+// changes the page title & text header (h3) to the correct year calendar on page load where text is 'bookingThisYear' or 'bookingNextYear'
+// therefore it only corrects the menu (nav a) on most pages but on the booking pages it changes the title and hearder text
+$('title, a, h3').contents().filter(function () {
+    return this.nodeType === 3;
+}).each(function () {
+    this.textContent = this.textContent.replace('bookingThisYear', '2018 Calendar');
+    this.textContent = this.textContent.replace('bookingNextYear', '2019 Calendar');
+});
